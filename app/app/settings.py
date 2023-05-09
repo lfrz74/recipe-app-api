ddcc030f34ef32eac3dd982b26cfab40f7ac9fb9
Fernando Rivera <anonimus47@hotmail.com>
2023-05-09 12:28:45 -0500

    Adding and modifying objects for deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lg467sqak-w%y3i91pmy#!sk6n!=ih+g+0+)$fl7(cxk5egmns'
+# SECRET_KEY = 'django-insecure-lg467sqak-w%y3i91pmy#!sk6n!=ih+g+0+)$fl7(cxk5egmns'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
